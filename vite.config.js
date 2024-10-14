afdb43a1b0157c44e7c55bfdc7bc876706489244
vasily88 <vasek88@gmail.com>
2024-10-14 20:07:20 +0300

    new package

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my_resume_by_react/dist/',
-  // base: './',
+  // base: '/my_resume_by_react/dist/',
+  base: './',
   plugins: [react()],
 })
